8cc4b04c304f337f2618fdc12817e8d9460e3a04
Samia Hanini <samia.hanini@condorcet.be>
2024-02-05 16:45:33 +0100

    second attempt API

--- app/pages/api/predict.ts
+++ app/pages/api/predict.ts
@@ -1,31 +1,38 @@
-// pages/api/predict.ts
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
   if (req.method === 'POST') {
     const file = req.body;
 
-    // Effectuez l'appel à l'API Custom Vision ici avec le fichier image
-    // Utilisez les clés et l'URL de prédiction
-
     const apiKey = '0b7c44517f024fa7bfbccd8080fea1c4';
     const endpoint = 'https://catsvsdogsprototype-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/39fe0cd0-a9dc-4b79-8d41-fa8c0064385f/classify/iterations/CatsVsDogsModel-1/image';
+    const iterationId = '3c465be5-82ea-4b37-b21c-05e15529553f';
 
-    const formData = new FormData();
-    formData.append('image', file);
+    try {
+      const response = await axios.post(
+        `${endpoint}/predict?iterationId=${iterationId}`,
+        file,
+        {
+          headers: {
+            'Prediction-Key': apiKey,
+            'Content-Type': 'application/octet-stream',
+          },
+        }
+      );
 
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Prediction-Key': apiKey,
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData,
-    });
-
-    const result = await response.json();
-    res.status(200).json(result);
+      const result = response.data;
+      res.status(200).json(result);
+    } catch (error) {
+      console.error('Error during prediction:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
-}
+};
+
+export default handler;
+
